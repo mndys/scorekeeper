3a08b0fc45b59e16aff4055a07bee15cba65d68d
Jeremias Erbs <jerry@badabam.de>
2021-02-19 17:13:58 +0100

    Integrate Player component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Player from './Player'
 import PlayerForm from './PlayerForm'
 
 function App() {
@@ -8,29 +9,38 @@ function App() {
   return (
     <div className="App">
       <PlayerForm onAddPlayer={addPlayer} />
+      {players.map(({ name, score, id }, index) => (
+        <Player
+          key={id}
+          name={name}
+          score={score}
+          onPlus={() => onPlus(index)}
+          onMinus={() => onMinus(index)}
+        />
+      ))}
       <button onClick={resetScores}>Reset scores</button>
       <button onClick={resetAll}>Reset all</button>
     </div>
   )
 
   function onPlus(index) {
-    setPlayers([
+    setPlayers(players => [
       ...players.slice(0, index),
-      {...players[index], score: players[index].score + 1},
-      ...players.slice(index + 1)
+      { ...players[index], score: players[index].score + 1 },
+      ...players.slice(index + 1),
     ])
   }
 
   function onMinus(index) {
-    setPlayers([
+    setPlayers(players => [
       ...players.slice(0, index),
-      {...players[index], score: players[index].score - 1},
-      ...players.slice(index + 1)
+      { ...players[index], score: players[index].score - 1 },
+      ...players.slice(index + 1),
     ])
   }
 
   function addPlayer(name) {
-    setPlayers([...players, { name }])
+    setPlayers([...players, { name, score: 0, id: players.length + 1 }])
   }
 
   function resetScores() {
