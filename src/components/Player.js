0ae1afacc70ee946c7e5080363b50bd732c1bc7a
macfrei <lene@7freiheiten.de>
2021-02-19 13:17:35 +0100

    Add player component with simple styles

---
+++ src/components/Player.js
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './Player.css'
+
+export default function Player({ name, score, onMinus, onPlus }) {
+  return (
+    <section className="Player">
+      <span>{name}</span>
+      <div className="Player__score">
+        <button onClick={onMinus}>-</button>
+        <span>{score}</span>
+        <button onClick={onPlus}>+</button>
+      </div>
+    </section>
+  )
+}
