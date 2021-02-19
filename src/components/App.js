d50f0fb9b83bb34f60dd466acf46ff303f9577cb
Jeremias Erbs <jerry@badabam.de>
2021-02-19 13:58:14 +0100

    Create App and PlayerForm

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,45 @@
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import PlayerForm from './PlayerForm'
 
 function App() {
+  const [players, setPlayers] = useState([])
+
   return (
     <div className="App">
-      Hello world
+      <PlayerForm onAddPlayer={addPlayer} />
+      <button onClick={resetScores}>Reset scores</button>
+      <button onClick={resetAll}>Reset all</button>
     </div>
-  );
+  )
+
+  function onPlus(index) {
+    setPlayers([
+      ...players.slice(0, index),
+      {...players[index], score: players[index].score + 1},
+      ...players.slice(index + 1)
+    ])
+  }
+
+  function onMinus(index) {
+    setPlayers([
+      ...players.slice(0, index),
+      {...players[index], score: players[index].score - 1},
+      ...players.slice(index + 1)
+    ])
+  }
+
+  function addPlayer(name) {
+    setPlayers([...players, { name }])
+  }
+
+  function resetScores() {
+    setPlayers(players.map(player => ({ ...player, score: 0 })))
+  }
+
+  function resetAll() {
+    setPlayers([])
+  }
 }
 
-export default App;
+export default App
